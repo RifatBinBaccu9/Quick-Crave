90edbfd36b83508adaf31ccbaaededae9c6841d3
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-10-14 16:47:12 +0600

    rift

--- resources/views/main-site/common/heading.blade.php
+++ resources/views/main-site/common/heading.blade.php
@@ -14,13 +14,13 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav  mx-auto ">
               <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="menu.html">Menu</a>
+                <a class="nav-link" href="">Menu</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="about.html">About</a>
+                <a class="nav-link" href="{{url('/about')}}">About</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="book.html">Book Table</a>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('main-site');
+    return view('main-site.pages.home');
+});
+Route::get('/about', function () {
+    return view('main-site.pages.about');
 });
