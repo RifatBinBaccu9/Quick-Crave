7384b66895371a027bb6147ea50851b1d2517ce9
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-10-15 22:35:05 +0600

    rfisd

--- resources/views/main-site/common/heading.blade.php
+++ resources/views/main-site/common/heading.blade.php
@@ -25,85 +25,76 @@
               <li class="nav-item">
                 <a class="nav-link" href="{{url('/book')}}">Book Table</a>
               </li>
-             
+        
+
+              <li class="nav-item dropdown">
+  <a class="nav-link nav-profile d-flex align-items-center" href="#" data-toggle="dropdown">
+    <img src="/admin-site/assets/img/profile-img.jpg" height="40px" alt="Profile" class="rounded-circle">
+    <span class="d-none d-md-block dropdown-toggle pl-2">K. Anderson</span>
+  </a>
+  <!-- End Profile Image Icon -->
+  <ul class="dropdown-menu dropdown-menu-right profile" style="z-index: 200;">
+    <li class="dropdown-header">
+      <h6>Kevin Anderson</h6>
+      <span>Web Designer</span>
+    </li>
+    <li>
+      <hr class="dropdown-divider">
+    </li>
+    <li>
+      <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+        <i class="fa fa-user"></i>
+        <span>My Profile</span>
+      </a>
+    </li>
+    <li>
+      <hr class="dropdown-divider">
+    </li>
+    <li>
+      <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+        <i class="fa fa-cog"></i>
+        <span>Account Settings</span>
+      </a>
+    </li>
+    <li>
+      <hr class="dropdown-divider">
+    </li>
+    <li>
+      <a class="dropdown-item d-flex align-items-center" href="#">
+        <i class="fa fa-sign-out-alt"></i>
+        <span>Sign Out</span>
+      </a>
+    </li>
+  </ul>
+  <!-- End Profile Dropdown Items -->
+</li>
+
             </ul>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav  mx-auto ">
-              <li class="nav-item">
-                <a class="nav-link" href="{{url('/signup')}}">Sign Up</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{url('/login')}}">LogIn</a>
-              </li>
-              </ul>
-            </div>
-            <div class="user_option">
-              <a href="" class="user_link">
-                <i class="fa fa-user" aria-hidden="true"></i>
-              </a>
-              <a class="cart_link" href="#">
-                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
-                  <g>
-                    <g>
-                      <path d="M345.6,338.862c-29.184,0-53.248,23.552-53.248,53.248c0,29.184,23.552,53.248,53.248,53.248
-                   c29.184,0,53.248-23.552,53.248-53.248C398.336,362.926,374.784,338.862,345.6,338.862z" />
-                    </g>
-                  </g>
-                  <g>
-                    <g>
-                      <path d="M439.296,84.91c-1.024,0-2.56-0.512-4.096-0.512H112.64l-5.12-34.304C104.448,27.566,84.992,10.67,61.952,10.67H20.48
-                   C9.216,10.67,0,19.886,0,31.15c0,11.264,9.216,20.48,20.48,20.48h41.472c2.56,0,4.608,2.048,5.12,4.608l31.744,216.064
-                   c4.096,27.136,27.648,47.616,55.296,47.616h212.992c26.624,0,49.664-18.944,55.296-45.056l33.28-166.4
-                   C457.728,97.71,450.56,86.958,439.296,84.91z" />
-                    </g>
-                  </g>
-                  <g>
-                    <g>
-                      <path d="M215.04,389.55c-1.024-28.16-24.576-50.688-52.736-50.688c-29.696,1.536-52.224,26.112-51.2,55.296
-                   c1.024,28.16,24.064,50.688,52.224,50.688h1.024C193.536,443.31,216.576,418.734,215.04,389.55z" />
-                    </g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                </svg>
-              </a>
-              <form class="form-inline">
-                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                  <i class="fa fa-search" aria-hidden="true"></i>
-                </button>
-              </form>
-              <a href="" class="order_online">
-                Order Online
-              </a>
-            </div>
+          @if (Auth::user())
+<!--           
+          <div class="user_option">
+            <a href="" class="user_link">
+              <i class="fa fa-user" aria-hidden="true"></i>
+            </a>
+            <a href="" class="order_online">
+              Order Online
+            </a>
+          </div> -->
+            
+          
+          @else
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           <ul class="navbar-nav  mx-auto ">
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('/signup')}}">Sign Up</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('/login')}}">LogIn</a>
+            </li>
+            </ul>
+          </div>
+          @endif
+            
           </div>
         </nav>
       </div>
