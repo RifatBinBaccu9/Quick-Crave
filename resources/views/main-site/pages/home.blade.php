760c559b36da310acf94d2b62fd7a9e8c5ec8652
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-10-15 12:36:37 +0600

    rifat

--- resources/views/main-site/pages/home.blade.php
+++ resources/views/main-site/pages/home.blade.php
@@ -994,37 +994,29 @@
       <div class="row">
         <div class="col-md-6">
           <div class="form_container">
-            <form action="">
+          <form action="{{ route('bookPush') }}" method="POST">
+              @csrf
               <div>
-                <input type="text" class="form-control" placeholder="Your Name" />
+                <input type="text" name="name" class="form-control" placeholder="Your Name" />
               </div>
               <div>
-                <input type="text" class="form-control" placeholder="Phone Number" />
+                <input type="text" name="phone" class="form-control" placeholder="Phone Number" />
               </div>
               <div>
-                <input type="email" class="form-control" placeholder="Your Email" />
+                <input type="text" name="email" class="form-control" placeholder="Your Email" />
               </div>
               <div>
-                <select class="form-control nice-select wide">
-                  <option value="" disabled selected>
+                <select name="persons" class="form-control nice-select wide">
+                  <option value="1" disabled selected>
                     How many persons?
                   </option>
-                  <option value="">
+                  <option value="2">
                     2
-                  </option>
-                  <option value="">
-                    3
-                  </option>
-                  <option value="">
-                    4
-                  </option>
-                  <option value="">
-                    5
                   </option>
                 </select>
               </div>
               <div>
-                <input type="date" class="form-control">
+                <input type="date" name="date" class="form-control">
               </div>
               <div class="btn_box">
                 <button>
