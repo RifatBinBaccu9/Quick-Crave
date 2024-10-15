58dfdbed74073293e65655a89c5cc64543528e00
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-10-16 00:22:59 +0600

    rif

--- resources/views/main-site/common/heading.blade.php
+++ resources/views/main-site/common/heading.blade.php
@@ -30,48 +30,91 @@
 
 
           @if (Auth::user())
-
-<div class="nav-item dropdown">
-  <a class="nav-link nav-profile d-flex align-items-center" href="#" data-toggle="dropdown">
-    <img src="/admin-site/assets/img/profile-img.jpg" height="40px" alt="Profile" class="rounded-circle">
-    <span class="d-none d-md-block dropdown-toggle pl-2">K. Anderson</span>
-  </a>
-  <!-- End Profile Image Icon -->
-  <ul class="dropdown-menu dropdown-menu-right profile" style="z-index: 200;">
-    <li class="dropdown-header">
-      <h6>Kevin Anderson</h6>
-      <span>Web Designer</span>
-    </li>
-    <li>
-      <hr class="dropdown-divider">
-    </li>
-    <li>
-      <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-        <i class="fa fa-user"></i>
-        <span class="ml-2">My Profile</span>
-      </a>
-    </li>
-    <li>
-      <hr class="dropdown-divider">
-    </li>
-    <li>
-      <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-        <i class="fa fa-cog"></i>
-        <span class="ml-2">Account Settings</span>
-      </a>
-    </li>
-    <li>
-      <hr class="dropdown-divider">
-    </li>
-    <li>
-      <a class="dropdown-item d-flex align-items-center" href="{{route('logout')}}">
-        <i class="fas fa-sign-out-alt"></i>
-        <span class="ml-2">Sign Out</span>
-      </a>
-    </li>
-  </ul>
-  <!-- End Profile Dropdown Items -->
-</div>
+            @if (Auth::user()->is_type == 'admin')
+            <div class="nav-item dropdown">
+              <a class="nav-link nav-profile d-flex align-items-center" href="#" data-toggle="dropdown">
+                <img src="/admin-site/assets/img/profile-img.jpg" height="40px" alt="Profile" class="rounded-circle">
+                <span class="d-none d-md-block dropdown-toggle pl-2">K. Anderson</span>
+              </a>
+              <!-- End Profile Image Icon -->
+              <ul class="dropdown-menu dropdown-menu-right profile" style="z-index: 200;">
+                <li class="dropdown-header">
+                  <h6>Kevin Anderson</h6>
+                  <span>Web Designer</span>
+                </li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li>
+                  <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                    <i class="fa fa-user"></i>
+                    <span class="ml-2">My Profile</span>
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li>
+                  <a class="dropdown-item d-flex align-items-center" href="{{route('admin')}}">
+                    <i class="fa fa-cog"></i>
+                    <span class="ml-2">Dashboard</span>
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li>
+                  <a class="dropdown-item d-flex align-items-center" href="{{route('logout')}}">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span class="ml-2">Sign Out</span>
+                  </a>
+                </li>
+              </ul>
+              <!-- End Profile Dropdown Items -->
+            </div>
+            @else
+            <div class="nav-item dropdown">
+              <a class="nav-link nav-profile d-flex align-items-center" href="#" data-toggle="dropdown">
+                <img src="/admin-site/assets/img/profile-img.jpg" height="40px" alt="Profile" class="rounded-circle">
+                <span class="d-none d-md-block dropdown-toggle pl-2">K. Anderson</span>
+              </a>
+              <!-- End Profile Image Icon -->
+              <ul class="dropdown-menu dropdown-menu-right profile" style="z-index: 200;">
+                <li class="dropdown-header">
+                  <h6>Kevin Anderson</h6>
+                  <span>Web Designer</span>
+                </li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li>
+                  <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                    <i class="fa fa-user"></i>
+                    <span class="ml-2">My Profile</span>
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li>
+                  <a class="dropdown-item d-flex align-items-center" href="#">
+                    <i class="fa fa-cog"></i>
+                    <span class="ml-2">Dashboard</span>
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li>
+                  <a class="dropdown-item d-flex align-items-center" href="{{route('logout')}}">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span class="ml-2">Sign Out</span>
+                  </a>
+                </li>
+              </ul>
+              <!-- End Profile Dropdown Items -->
+            </div>
+            @endif
           
           @else
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
