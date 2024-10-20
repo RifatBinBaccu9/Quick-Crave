c3f13e72f3f3262720e2c29e19e4fa77d2bc2851
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-10-20 23:39:53 +0600

    rifat

--- resources/views/admin-site/pages/profile.blade.php
+++ resources/views/admin-site/pages/profile.blade.php
@@ -21,12 +21,12 @@
             <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
 
               <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-              <h2>Kevin Anderson</h2>
-              <h3>Web Designer</h3>
+              <h2>{{$users->name}}</h2>
+              <h3>{{$users->email}}</h3>
               <div class="social-links mt-2">
-                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                <a href="{{$users->twitter}}" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="{{$users->facebook}}" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="{{$users->linkedin}}" class="linkedin"><i class="bi bi-linkedin"></i></a>
               </div>
             </div>
           </div>
